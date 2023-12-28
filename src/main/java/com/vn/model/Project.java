97bb15eca8aabf85db6fc14b6b0b2a1d28ed2b93
IHau <l3abysexy@gmail.com>
2023-12-28 23:49:58 +0700

    Implement the create project interface - Note: Incomplete

--- src/main/java/com/vn/model/Project.java
+++ src/main/java/com/vn/model/Project.java
@@ -47,6 +47,7 @@ public class Project {
         if (id != project.id) return false;
         if (!Objects.equals(name, project.name)) return false;
         if (!Objects.equals(startDate, project.startDate)) return false;
+        if (!Objects.equals(code, project.code)) return false;
         return Objects.equals(endDate, project.endDate);
     }
 
@@ -56,6 +57,7 @@ public class Project {
         result = 31 * result + (name != null ? name.hashCode() : 0);
         result = 31 * result + (startDate != null ? startDate.hashCode() : 0);
         result = 31 * result + (endDate != null ? endDate.hashCode() : 0);
+        result = 31 * result + (code != null ? code.hashCode() : 0);
         return result;
     }
 }
