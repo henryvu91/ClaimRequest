28f873ebdceab31a953397fb44853ea3d2bf0e1e
dongkakaka <nguyenthedong25042003@gmail.com>
2023-12-29 15:25:01 +0700

    add UI create Claim chua hoan thien. view list làm gần hoàn thiện. thiếu phân trang

--- src/main/java/com/vn/controller/ClaimController.java
+++ src/main/java/com/vn/controller/ClaimController.java
@@ -1,11 +1,13 @@
 package com.vn.controller;
 
-import com.vn.model.Claim;
+import com.vn.dto.ClaimDTO;
 import com.vn.services.ClaimServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -15,14 +17,23 @@ public class ClaimController {
     private ClaimServiceImpl claimServiceImpl;
 
     @GetMapping("/create")
-    public String createClaim(Model model) {
+    public String createClaimUI(Model model) {
         if (model.containsAttribute("message")) {
             String message = (String) model.asMap().get("message");
             model.addAttribute("message", message);
         }
-        model.addAttribute("addClaimForm", new Claim());
+
+
+        model.addAttribute("addClaimFrom", new ClaimDTO());
         return "view/claim/create";
     }
+
+    @PostMapping("/create")
+    public String createClaim(@ModelAttribute("addClaimFrom") ClaimDTO claimDTO) {
+        System.out.println(claimDTO);
+        return "view/claim/create";
+    }
+
 //    @GetMapping("/delete/{id}")
 //    public String deleteClaim(@PathVariable("id") int id, Model model) {
 //        claimServiceImpl.deleteClaim(id);
--- src/main/resources/static/javascript/claim/create-claim.js
+++ src/main/resources/static/javascript/claim/create-claim.js
@@ -1,76 +1,151 @@
 $(document).ready(function () {
-    // Cached jQuery selectors
-    var $startDateInput = $('#form-addClaimDetail__startDate');
-    var $endDateInput = $('#form-addClaimDetail__endDate');
-    var $dateInput = $('#form-addClaimDetail__date');
-    var $dayInput = $('#form-addClaimDetail__day');
-    var $durationInput = $('#form-addClaim__project-duration');
-    var $fromTime = $('#form-addClaimDetail__fromTime');
-    var $toTime = $('#form-addClaimDetail__toTime');
-    var $totalHours = $('#form-addClaimDetail__totalHours');
-    var $dateValidationMessage = $("#dateValidationMessage");
-    var $auditTrail = $('#form-addClaimDetail__audit-trail');
+    let dateInput = $('#form-addClaimDetail__date');
 
-    // Update day name when date changes
     function updateDay() {
-        let selectedDate = new Date($dateInput.val());
-        let day = selectedDate.toLocaleDateString('en-US', {weekday: 'long'});
-        $dayInput.val(day);
+        let dayInput = $('#form-addClaimDetail__day');
+
+        let selectedDate = new Date(dateInput.val());
+        let options = {weekday: 'long'};
+        let day = selectedDate.toLocaleDateString('en-US', options);
+
+        dayInput.val(day);
     }
 
-    // Calculate and update duration
+    dateInput.on('change', function () {
+        updateDay();
+    });
+
+    // Logic sử lí tính Duration Project
     function calculateDuration() {
-        let startDate = new Date($startDateInput.val());
-        let endDate = new Date($endDateInput.val());
+        let startDateInput = $('#form-addClaimDetail__startDate');
+        let endDateInput = $('#form-addClaimDetail__endDate');
+        let durationInput = $('#form-addClaim__project-duration');
+
+        let startDate = new Date(startDateInput.val());
+        let endDate = new Date(endDateInput.val());
+
         let duration = Math.abs(endDate - startDate);
         let days = Math.ceil(duration / (1000 * 60 * 60 * 24));
-        $durationInput.val(days + ' days');
+
+        durationInput.val(days + ' days');
     }
 
-    // Calculate and update total hours
-    function calculateTotalHours() {
-        var fromTimeObj = new Date("1970-01-01T" + $fromTime.val() + "Z");
-        var toTimeObj = new Date("1970-01-01T" + $toTime.val() + "Z");
-        var totalHours = Math.round(((toTimeObj - fromTimeObj) / (1000 * 60 * 60)) * 100) / 100;
-        $totalHours.val(totalHours);
-    }
-
-    // Validate selected date within the range of start and end dates
-    function validateDate() {
-        var startDate = $startDateInput.val();
-        var endDate = $endDateInput.val();
-        var dateInput = $dateInput.val();
-        if (dateInput < startDate || dateInput > endDate) {
-            $dateValidationMessage.text("Date must be within the range of Start Date and End Date");
-        } else {
-            $dateValidationMessage.text("");
-        }
-    }
-
-    // Event handlers
-    $dateInput.on('change', function () {
-        updateDay();
-        validateDate();
-    });
-    $startDateInput.on('change', calculateDuration);
-    $endDateInput.on('change', function () {
+    $('#form-addClaimDetail__startDate, #form-addClaimDetail__endDate').on('change', function () {
         calculateDuration();
-        validateDate();
     });
-    $fromTime.on('change', calculateTotalHours);
-    $toTime.on('change', calculateTotalHours);
 
-    // Save form data and display audit trail on save
-    $('#saveBtn').on('click', function (e) {
-        e.preventDefault();
-        // Save form data logic here...
+    // Logic tính total hours
+    function calculateTotalHours() {
+        var fromTime = $('#form-addClaimDetail__fromTime').val();
+        var toTime = $('#form-addClaimDetail__toTime').val();
+        var fromTimeObj = new Date("1970-01-01T" + fromTime + "Z");
+        var toTimeObj = new Date("1970-01-01T" + toTime + "Z");
 
-        // Audit trail
+        var totalHours = (toTimeObj - fromTimeObj) / (1000 * 60 * 60);
+        totalHours = Math.round(totalHours * 100) / 100;
+        $('#form-addClaimDetail__totalHours').val(totalHours);
+    }
+
+    $('#form-addClaimDetail__fromTime, #form-addClaimDetail__toTime').on('change', function () {
+        calculateTotalHours();
+    });
+
+    // Hiển thị audit trail
+    $('#saveBtn').on('click', function () {
+        alert("Thành công");
         let currentTime = new Date();
         let formattedTime = currentTime.toLocaleString();
-        let creatorName = 'YourName'; // Replace with actual variable or logic to get creator's name
-        $auditTrail.val("Created by " + creatorName + " on " + formattedTime);
+        $('#form-addClaimDetail__audit-trail').val("Created by " + creatorName + " on " + formattedTime);
+    });
+});
+// lưu thông tin nhập
+$(document).ready(function () {
+    $('#saveBtn').on('click', function (e) {
+        e.preventDefault();
 
+        // Lấy giá trị của các trường nhập liệu
+        var status = $('#form-addClaim__status').val();
+        var staffId = $('#form-addClaim__staff-id').val();
+        var staffName = $('#form-addClaim__staff-name').val();
+        var staffDepartment = $('#form-addClaim__staff-department').val();
+        var projectName = $('#form-addClaim__project-name').val();
+        var startDate = $('#form-addClaimDetail__startDate').val();
+        var endDate = $('#form-addClaimDetail__endDate').val();
+        var projectDuration = $('#form-addClaim__project-duration').val();
+        var claimDetailDate = $('#form-addClaimDetail__date').val();
+        var claimDetailDay = $('#form-addClaimDetail__day').val();
+        var claimDetailFromTime = $('#form-addClaimDetail__fromTime').val();
+        var claimDetailToTime = $('#form-addClaimDetail__toTime').val();
+        var claimDetailTotalHours = $('#form-addClaimDetail__totalHours').val();
+        var remarks = $('#form-addClaimDetail__remarks').val();
+        var auditTrail = $('#form-addClaimDetail__audit-trail').val();
+
+        // Lưu các giá trị vào localStorage hoặc thực hiện các thao tác lưu dữ liệu khác
+        localStorage.setItem('status', status);
+        localStorage.setItem('staffId', staffId);
+        localStorage.setItem('staffName', staffName);
+        localStorage.setItem('staffDepartment', staffDepartment);
+        localStorage.setItem('projectName', projectName);
+        localStorage.setItem('startDate', startDate);
+        localStorage.setItem('endDate', endDate);
+        localStorage.setItem('projectDuration', projectDuration);
+        localStorage.setItem('claimDetailDate', claimDetailDate);
+        localStorage.setItem('claimDetailDay', claimDetailDay);
+        localStorage.setItem('claimDetailFromTime', claimDetailFromTime);
+        localStorage.setItem('claimDetailToTime', claimDetailToTime);
+        localStorage.setItem('claimDetailTotalHours', claimDetailTotalHours);
+        localStorage.setItem('remarks', remarks);
+        localStorage.setItem('auditTrail', auditTrail);
+
+        // Hiển thị thông báo thành công
         alert('Data saved successfully!');
     });
 });
+
+//  bắt validate chọn Date trong khoảng Start DATE VÀ end date
+function validateDate() {
+    var startDate = $("#form-addClaimDetail__startDate").val();
+    var endDate = $("#form-addClaimDetail__endDate").val();
+    var dateInput = $("#form-addClaimDetail__date").val();
+    var dateValidationMessage = $("#dateValidationMessage");
+
+    if (dateInput < startDate || dateInput > endDate) {
+        dateValidationMessage.text("Date must be within the range of Start Date and End Date");
+    } else {
+        dateValidationMessage.text("");
+    }
+}
+$("#form-addClaim").validate({
+    errorClass: "is-invalid",
+    validClass: "is-valid",
+    errorElement: "div",
+    errorPlacement: function (error, element) {
+      error.addClass("invalid-feedback");
+      if (element.prop("type") === "checkbox") {
+        error.insertAfter(element.next("label"));
+      } else {
+        error.insertAfter(element);
+      }
+    },
+    highlight: function (element, errorClass, validClass) {
+      $(element).addClass(errorClass).removeClass(validClass);
+    },
+    unhighlight: function (element, errorClass, validClass) {
+      $(element).removeClass(errorClass).addClass(validClass);
+    },
+    rules: {
+        remarks: {
+        required: true,
+      },
+      
+    },
+    messages: {
+      remarks: {
+        required: "Please enter remarks",
+      },
+      
+    },
+    submitHandler: function (form) {
+      form.submit();
+    },
+  });
