021a7761cf95fee78f89e84bffb6efac918befe2
vudacviethung@gmail.com <vudacviethung@gmail.com>
2024-01-07 22:51:43 +0700

    -Make approve, reject, return -Make paid, reject

--- src/main/java/com/vn/repositories/ClaimRepository.java
+++ src/main/java/com/vn/repositories/ClaimRepository.java
@@ -1,13 +1,10 @@
 package com.vn.repositories;
 
-import com.vn.dto.view.ClaimTotalDTO;
 import com.vn.model.Claim;
 import com.vn.utils.Status;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
